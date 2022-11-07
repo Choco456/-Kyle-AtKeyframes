c72ca1f50d4b61ef21fe2aca7fed423a2ff7a789
Choco456 <113038136+Choco456@users.noreply.github.com>
2022-11-07 09:43:53 -0500

    second commit

---
+++ .js
@@ -0,0 +1,16 @@
+var boxOne = document.getElementsByClassName('box')[0];
+var running = false;
+
+var toggleTransition = function() {
+  if(!running) 
+  { 
+    boxOne.classList.add('horizTranslate');
+  } else {
+    boxOne.classList.remove('horizTranslate'); 
+  }  
+
+  running = !running;
+}
+
+var boxOne = doceument.getElementsByClassName('box'){0};
+vae runing = false
